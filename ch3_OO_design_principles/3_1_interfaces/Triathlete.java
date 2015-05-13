0f3d949237a31aac80ae3a4e2f415ce4450b37ec
Kevin Gracie <kgracie12@gmail.com>
2015-05-13 20:45:31 +0000

    Added broken case

--- ch3_OO_design_principles/3_1_interfaces/Triathlete.java
+++ ch3_OO_design_principles/3_1_interfaces/Triathlete.java
@@ -10,10 +10,16 @@ public class Triathlete implements Cyclist, Swimmer, Runner {
         System.out.println("Swimming");
     }
      
+    /**
+     * Commenting out the run method means this class is failing to implement all of the methods of
+     * all of the interfaces it has agreed to implement. This will cause a compilation failure.
     @Override
     public void run() {
         System.out.println("Running");
     }
+    */
+    
+    
     
     
 
