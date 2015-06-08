d9ac8cca064864a789f071d6b2a5727f1aea0ffa
Kevin Gracie <kgracie12@gmail.com>
2015-06-08 21:07:51 +0000

    Fixed typos

--- ch4_generics_and_collections/4_4_interop_collections_raw_types/GenericRawAssignment.java
+++ ch4_generics_and_collections/4_4_interop_collections_raw_types/GenericRawAssignment.java
@@ -7,7 +7,7 @@ public class GenericRawAssignment {
          */
         
         Movable movable = new Movable<Car>();   // This line compiles without error or warning.
-        movable.set(new Car());                 // Compiles with unchecked warning - raw type loses type information of movable so compiler can check for type safety
+        movable.set(new Car());                 // Compiles with unchecked warning - raw type loses type information of movable so compiler can't check for type safety
         //Car car = movable.get();                // Compiler error - type information not set on Movable so T is of type Object. Cannot be assigned without explicit cast
         Car car = (Car) movable.get();          // This line replaces one above to ensure successful compilation.
         
@@ -17,7 +17,7 @@ public class GenericRawAssignment {
          */
         
         Movable<Car> anotherMovable = new Movable();    // Unchecked conversion warning
-        anotherMovable.set(new Car());      // No warnings here We have type information for anotherMovable so the compiler is able to verify the object we are passing as argument to the method
+        anotherMovable.set(new Car());      // No warnings here. We have type information for anotherMovable so the compiler is able to verify the object we are passing as argument to the method
         Car anotherCar = movable.get();     // No need for explicit cast here - we have type information for movable so will know what is returned from get() is of type Car so can be assigned to variable of type Car
     
     
